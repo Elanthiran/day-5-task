11d52ebaebf666e03f5725a20a48662c2c92df88
Elanthiran <elanthirank@gmail.com>
2024-01-01 11:47:32 +0530

    JSON formats

---
+++ Resume.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basic details": {
+      "name": "ELANTHIRAN K",
+      "email": "elanthirank@gmail.com",
+      "phone": 6382079953 & 8608579953,
+        "degree": "B.E",
+      "DOB":16/6/2000,
+      "location": {
+        "address": "113,Muniyappan Kovil Street,Kitchipalayam",
+        "postalCode": 636015,
+        "city": "Salem",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+            "website": "https://www.linkedin.com/in/elanthiran-k-92809b214",
+            "github":"https://github.com/Elanthiran/day-5-task.git"
+        }
+      ]
+    },
+    "education": [
+      {
+        "institution": "Government College Engineering ,Salem",
+        "department": "Mechanical",
+        "studyType": "fulltime",
+        "batch start year": 2018,
+        "batch end year": 2022,
+        "gpa": 8.5,
+      }
+    ],
+    "Workshop": [
+        {
+            2019: "Participated in MATLAB workshop conducted by Mahindra - NIT Trichy",
+            2020: "Participated in ELECTRIC VECHILE workshop conducted by SYNERGY-NIT Trichy"
+            
+        }
+    ],
+    "Competencies": [
+      {
+        "name": "Html,Javascript,python,Ansys",
+        "level": "beginer",
+            "project": [
+                "1.Design and fabrication of multiple application angle grinder machine",
+                "2.Design and performance analysis of Peltier module for thermal conditions in a room"
+            ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Cricket,Chess,Story Writing",
+      }
+    ]
+  }
+  console.log(myResume);
